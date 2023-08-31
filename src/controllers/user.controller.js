c1a26980e7b4e8388d54afcfea44f24e89633864
Kauã Xavier <kauaxavier032@gmail.com>
2023-08-31 20:56:11 -0300

    feat: create new routes

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,4 +1,5 @@
 const userService = require('../services/user.service');
+const mongoose = require('mongoose');
 
 const create = async (req, res) => {
     const {name, username, email, password, avatar, background} = req.body;
@@ -7,7 +8,7 @@ const create = async (req, res) => {
         res.status(400).send({"message" : "Submit all fields for registration"});
     }
 
-    const user = await userService.create(req.body);
+    const user = await userService.createService(req.body);
 
     if(!user){
         return res.status(400).send({message: "Error creating User"});
@@ -26,4 +27,30 @@ const create = async (req, res) => {
     });
 };
 
-module.exports = {create};
+const findAll = async (req, res) => {
+    const users = await userService.findAllService();
+
+    if(users.lenght === 0){
+        return res.status(400).send({message : "There are no registered users"});
+    }
+
+    res.send(users);
+};
+
+const findById = async (req, res) => {
+    const id = req.params.id;
+
+    if(!mongoose.Types.ObjectId.isValid(id)){
+        return res.status(400).send({ message : "Invalid ID"})
+    }
+
+    const user = await userService.findByIdService(id);
+
+    if(!user){
+        return res.status(400).send({ message: "User not found"});
+    }
+
+    res.send(user);
+}
+
+module.exports = {create, findAll, findById};
