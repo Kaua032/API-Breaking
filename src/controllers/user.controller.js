6132dddc5f6eab36a852c3a242d7e451d1bd0afa
Kauã Xavier <kauaxavier032@gmail.com>
2023-09-07 16:46:33 -0300

    feat: created global middlewares

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,5 +1,4 @@
 const userService = require('../services/user.service');
-const mongoose = require('mongoose');
 
 const create = async (req, res) => {
     const {name, username, email, password, avatar, background} = req.body;
@@ -38,17 +37,7 @@ const findAll = async (req, res) => {
 };
 
 const findById = async (req, res) => {
-    const id = req.params.id;
-
-    if(!mongoose.Types.ObjectId.isValid(id)){
-        return res.status(400).send({ message : "Invalid ID"})
-    }
-
-    const user = await userService.findByIdService(id);
-
-    if(!user){
-        return res.status(400).send({ message: "User not found"});
-    }
+    const { id, user } = req;
 
     res.send(user);
 }
@@ -60,17 +49,7 @@ const update = async (req, res) =>{
         res.status(400).send({"message" : "Submit ar least one field for update"});
     }
     
-    const id = req.params.id;
-
-    if(!mongoose.Types.ObjectId.isValid(id)){
-        return res.status(400).send({ message: "Invalid ID" });
-    }
-
-    const user = await userService.findByIdService(id);
-
-    if(!user){
-        return res.status(400).send({ message: "User not found" });
-    }
+    const id = req.id;
 
     await userService.updateService( id, name, username, email, password, avatar, background );
 
