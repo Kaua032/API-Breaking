2d75c54985f2171fb94907d79821c476b7445045
Kauã Xavier <kauaxavier032@gmail.com>
2023-09-11 19:29:40 -0300

    feat:

--- src/controllers/news.controller.js
+++ src/controllers/news.controller.js
@@ -2,6 +2,7 @@ import {
   createService,
   findAllService,
   countNews,
+  topNewsService,
 } from "../services/news.service.js";
 
 const create = async (req, res) => {
@@ -28,53 +29,85 @@ const create = async (req, res) => {
 };
 
 const findAll = async (req, res) => {
-  let { limit, offset } = req.query;
+  try {
+    let { limit, offset } = req.query;
 
-  limit = Number(limit);
-  offset = Number(offset);
+    limit = Number(limit);
+    offset = Number(offset);
 
-  if (!limit) {
-    limit = 5;
+    if (!limit) {
+      limit = 5;
+    }
+
+    if (!offset) {
+      offset = 0;
+    }
+
+    const news = await findAllService(offset, limit);
+    const total = await countNews();
+    const currentUrl = req.baseUrl;
+
+    const next = offset + limit;
+    const nextUrl =
+      next < total ? `${currentUrl}?limit=${limit}&offset=${next}` : null;
+
+    const previous = offset - limit < 0 ? null : offset - limit;
+    const previousUrl =
+      previous != null
+        ? `${currentUrl}?limit=${limit}&offset=${previous}`
+        : null;
+
+    if (news.length === 0) {
+      return res.status(400).send({ message: "There are no registered news" });
+    }
+
+    res.send({
+      nextUrl,
+      previousUrl,
+      limit,
+      offset,
+      total,
+      results: news.map((newsItem) => ({
+        id: newsItem._id,
+        title: newsItem.title,
+        text: newsItem.text,
+        banner: newsItem.banner,
+        likes: newsItem.likes,
+        comments: newsItem.comments,
+        name: newsItem.user.name,
+        userName: newsItem.user.username,
+        userAvatar: newsItem.user.avatar,
+      })),
+    });
+  } catch (err) {
+    res.status(500).send({ message: err.message });
   }
-
-  if (!offset) {
-    offset = 0;
-  }
-
-  const news = await findAllService(offset, limit);
-  const total = await countNews();
-  const currentUrl = req.baseUrl;
-
-  const next = offset + limit;
-  const nextUrl =
-    next < total ? `${currentUrl}?limit=${limit}&offset=${next}` : null;
-
-  const previous = offset - limit < 0 ? null : offset - limit;
-  const previousUrl =
-    previous != null ? `${currentUrl}?limit=${limit}&offset=${previous}` : null;
-
-  if (news.length === 0) {
-    return res.status(400).send({ message: "There are no registered news" });
-  }
-
-  res.send({
-    nextUrl,
-    previousUrl,
-    limit,
-    offset,
-    total,
-    results: news.map((newsItem) => ({
-      id: newsItem._id,
-      title: newsItem.title,
-      text: newsItem.text,
-      banner: newsItem.banner,
-      likes: newsItem.likes,
-      comments: newsItem.comments,
-      name: newsItem.user.name,
-      userName: newsItem.user.username,
-      userAvatar: newsItem.user.avatar,
-    })),
-  });
 };
 
-export { create, findAll };
+const topNews = async (req, res) => {
+  try {
+    const news = await topNewsService();
+
+    if (!news) {
+      return res.status(400).send({ message: "There is no registred post" });
+    }
+
+    res.send({
+      news: {
+        id: news._id,
+        title: news.title,
+        text: news.text,
+        banner: news.banner,
+        likes: news.likes,
+        comments: news.comments,
+        name: news.user.name,
+        userName: news.user.username,
+        userAvatar: news.user.avatar,
+      },
+    });
+  } catch (err) {
+    res.status(500).send({ message: err.message });
+  }
+};
+
+export { create, findAll, topNews };
