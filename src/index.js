f18d7de7a2b018f50f4ca9ec43309438b02f787e
Kauã Xavier <kauaxavier032@gmail.com>
2023-09-18 21:40:12 -0300

    feat: Update dependencies and add Swagger UI

---
+++ src/index.js
@@ -0,0 +1,21 @@
+import express from "express";
+import connectDatabase from "./database/db.js";
+
+import userRoute from "./routes/user.route.js";
+import authRoute from "./routes/auth.route.js";
+import newsRoute from "./routes/news.route.js";
+import swaggerRoute from "./routes/swagger.route.js";
+
+const PORT = 3500;
+const app = express();
+
+connectDatabase();
+app.use(express.json());
+app.use("/user", userRoute);
+app.use("/auth", authRoute);
+app.use("/news", newsRoute);
+app.use("/doc", swaggerRoute );
+
+app.listen(PORT, () => {
+  console.log(`Server On 🟢 ${PORT}`);
+});
